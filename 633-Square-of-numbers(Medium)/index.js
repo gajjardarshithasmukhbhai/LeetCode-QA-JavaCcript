e9533b5b316da7b2d78182aaf9ecc3f3f33e6a82
gajjar darshit hasmukhbhai <34138770+gajjardarshithasmukhbhai@users.noreply.github.com>
2024-06-18 01:10:56 +0530

    multiple Pointer time comeplexity is O(n)

--- 633-Square-of-numbers(Medium)/index.js
+++ 633-Square-of-numbers(Medium)/index.js
@@ -13,3 +13,37 @@ const isSquareOrNot = (value) => {
 }
 
 console.log(isSquareOrNot(5));
+
+
+// Below used the Multiple Pointer Method, Time Complexiety is O(n)
+const isSquareOrNot = (value) => {
+    // multiple pointer
+    let left = 0;
+    let right = Math.ceil(Math.sqrt(value));
+    while (left <= right) {
+        let total = left * left + right * right;
+        if (total === value) {
+            return true;
+        }
+        if (total < value) {
+            left++;
+        }
+        if (total > value) {
+            right--;
+        }
+    }
+    return false;
+}
+console.log(isSquareOrNot(2));
+
+// One More Cool Solution with very Optimize Complexiety is in Below(Found from others solution)
+const isSquareOrNot = (c) => {
+    for (let a = 0; a * a <= c; a++) {
+        let b = Math.sqrt(c - a * a);
+        if (b === Math.floor(b)) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(isSquareOrNot(4));
