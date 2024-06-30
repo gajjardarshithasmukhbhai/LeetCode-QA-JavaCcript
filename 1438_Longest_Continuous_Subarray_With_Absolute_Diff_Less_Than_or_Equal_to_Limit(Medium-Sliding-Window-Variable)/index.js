ae8dab359e10f65b31db2cc1171e96f3b6317c2a
gajjar darshit hasmukhbhai <34138770+gajjardarshithasmukhbhai@users.noreply.github.com>
2024-07-01 00:17:18 +0530

    1438-add

---
+++ 1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit(Medium-Sliding-Window-Variable)/index.js
@@ -0,0 +1,68 @@
+// Brut Force Approach, is simple for this
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+let nums = [10, 1, 2, 4, 7, 2];
+let limit = 5;
+var longestSubarray = function (nums, limit) {
+    let maxLength = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let min = nums[i];
+        let max = nums[i];
+        for (let j = i; j < nums.length; j++) {
+            if (nums[j] < min) {
+                min = nums[j];
+            }
+            if (nums[j] > max) {
+                max = nums[j];
+            }
+            if (max - min <= limit) {
+                maxLength = Math.max(maxLength, j - i + 1);
+            }
+        }
+    }
+    return maxLength;
+};
+console.log(longestSubarray(nums, limit));
+
+
+// Vaiable sliding window Approach
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+var longestSubarray = function (nums, limit) {
+    let i = 0;
+    let j = 0;
+    let minqueue = []; // In minQueue goes from increasing order
+    let maxqueue = []; // In maxQueue goes decreasing order
+    let maxLength = 0;
+    while (j < nums.length) {
+        while (maxqueue.length > 0 && nums[j] > maxqueue[maxqueue.length - 1]) {
+            maxqueue.pop();
+        }
+        maxqueue.push(nums[j]);
+        while (minqueue.length > 0 && nums[j] < minqueue[minqueue.length - 1]) {
+            minqueue.pop();
+        }
+        minqueue.push(nums[j]);
+        // main logic
+        while (maxqueue[0] - minqueue[0] > limit) {
+            if (nums[i] === maxqueue[0]) {
+                maxqueue.shift();
+            }
+            if (nums[i] === minqueue[0]) {
+                minqueue.shift();
+            }
+            i++;
+        }
+        maxLength = Math.max(maxLength, j - i + 1);
+        j++;
+    }
+    return maxLength;
+};
+
+// Note: 41 to 28 become same if value need min,max or only max longestSubarray. but after code Where logic is started
+// nums[i] work like pointer and we used that to make our i => i++ increase so that, we got the window frame
+
+// This is Variable Sliding Algorithm 
