152b57d57f87388e992f16b9ede67cc7f0cef45b
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2024-11-26 02:02:57 +0530

    add optimized function

--- Daily-LeetCode-Challanges/1072_Flip_Columns_For_Maximum_Number_of_Equal_Rows/optimized-PS.js
+++ Daily-LeetCode-Challanges/1072_Flip_Columns_For_Maximum_Number_of_Equal_Rows/optimized-PS.js
@@ -23,3 +23,21 @@ var maxEqualRowsAfterFlips = function (matrixValues) {
     }
     return maxValueReturn
 };
+
+// Top Solution with Less TC and SC
+
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var maxEqualRowsAfterFlips = function (matrix) {
+    const map = new Map();
+
+    for (let row of matrix) {
+        const pattern = row[0] ? row.map(bit => bit ^ 1).join('') : row.join('');
+
+        map.set(pattern, (map.get(pattern) || 0) + 1);
+    }
+
+    return Math.max(...Array.from(map.values()))
+};
