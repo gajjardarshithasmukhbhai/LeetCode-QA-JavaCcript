1092363ef2546314308fd4e2e95ce8d4ae92e287
gajjardarshithasmukhbhai <darshit.hasmukhbhai1998@gmail.com>
2024-12-03 23:11:47 +0530

    new problem solved

---
+++ Daily-LeetCode-Challanges/2109_Adding_Spaces_To_A_String/optimized.js
@@ -0,0 +1,33 @@
+// with  slice approach and beats 100% peoples
+/**
+ * @param {string} s
+ * @param {number[]} spaces
+ * @return {string}
+ */
+var addSpaces = function (s, spaces) {
+    const finalResult = [];
+    let startingIndex = 0;
+    for (let i = 0; i <= spaces.length; i++) {
+        finalResult.push(s.slice(startingIndex, spaces[i]));
+        startingIndex = spaces[i];
+    }
+    return finalResult.join(' ');
+};
+
+// multi pointer approach
+const s = "icodeinpython";
+const spaces = [1, 5, 7, 9];
+const addSpaces = (s, spaces) => {
+    let result = "";
+    let startIndex = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (i === spaces[startIndex]) {
+            result += " ";
+            startIndex++;
+        }
+        result += s[i];
+    }
+    return result;
+}
+console.log(addSpaces(s, spaces));
